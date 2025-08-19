0fa5ef286de52613aacf0ec03a631eed28274eeb
이민우 <imin-u@imin-uui-MacBookPro.local>
2025-08-19 19:46:56 +0900

    style: vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
 
 // https://vite.dev/config/
 import path from 'node:path';
@@ -10,7 +11,12 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dts(), react()],
+  resolve: {
+    alias: {
+      '~': path.resolve(dirname, 'src'),
+    },
+  },
   test: {
     projects: [
       {
