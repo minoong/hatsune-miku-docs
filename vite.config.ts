e217c63c03c069dc446bd74ebd7e9e49bb23dfad
이민우 <59505318+minoong@users.noreply.github.com>
2025-08-19 20:49:29 +0900

    build: tailwind (#1)
    Co-authored-by: 이민우 <imin-u@imin-uui-MacBookPro.local>

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import dts from 'vite-plugin-dts';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 import path from 'node:path';
@@ -11,7 +12,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
-  plugins: [dts(), react()],
+  plugins: [dts(), tailwindcss(), react()],
   resolve: {
     alias: {
       '~': path.resolve(dirname, 'src'),
